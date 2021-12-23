b7473c33c279ab98eaaa9c5d3f2d9ae2bdebeb0f
Jesse Guerrero <jesseguerrero1991@gmail.com>
2021-12-22 20:42:48 -0600

    init express

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+express.static.mime.types['wasm'] = 'application/wasm'
+
+var app = express();
+const webPort = 80;
+
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+/* GET home page. */
+app.get('/', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+app.post('/posty', (request, response) => {
+  console.log(request.body)
+})
+
+app.get('/work', function(req, res, next) {
+  res.send("test")
+});
+
+app.listen(webPort, "0.0.0.0", ()=> console.log("Successful on port " + webPort))
+
+
+
+
+module.exports = app;
