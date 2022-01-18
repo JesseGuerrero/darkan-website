8e6ddc3188b9f40facb118d12a8b0642cc58ff38
Andrew Gonzales <78248811+agonzales95@users.noreply.github.com>
2022-01-18 15:10:14 -0600

    skill-calc
    added skilling calculator

--- app.js
+++ app.js
@@ -61,6 +61,106 @@ app.get('/skill-calculator', function(req, res, next) {
   res.render("skill-calculator", {})
 });
 
+  app.get('/skill-calculator-attack', function(req, res, next) {
+    res.render("skill-calculator-attack", {})
+  });
+
+  app.get('/skill-calculator-strength', function(req, res, next) {
+    res.render("skill-calculator-strength", {})
+  });
+
+  app.get('/skill-calculator-defense', function(req, res, next) {
+    res.render("skill-calculator-defense", {})
+  });
+
+  app.get('/skill-calculator-range', function(req, res, next) {
+    res.render("skill-calculator-range", {})
+  });
+
+  app.get('/skill-calculator-magic', function(req, res, next) {
+    res.render("skill-calculator-magic", {})
+  });
+
+  app.get('/skill-calculator-hitpoints', function(req, res, next) {
+    res.render("skill-calculator-hitpoints", {})
+  });
+
+  app.get('/skill-calculator-prayer', function(req, res, next) {
+    res.render("skill-calculator-prayer", {})
+  });
+
+  app.get('/skill-calculator-summoning', function(req, res, next) {
+    res.render("skill-calculator-summoning", {})
+  });
+
+  app.get('/skill-calculator-runecrafting', function(req, res, next) {
+    res.render("skill-calculator-runecrafting", {})
+  });
+
+  app.get('/skill-calculator-construction', function(req, res, next) {
+    res.render("skill-calculator-construction", {})
+  });
+
+  app.get('/skill-calculator-dungeoneering', function(req, res, next) {
+    res.render("skill-calculator-dungeoneering", {})
+  });
+
+  app.get('/skill-calculator-agility', function(req, res, next) {
+    res.render("skill-calculator-agility", {})
+  });
+
+  app.get('/skill-calculator-herblore', function(req, res, next) {
+    res.render("skill-calculator-herblore", {})
+  });
+
+  app.get('/skill-calculator-theiving', function(req, res, next) {
+    res.render("skill-calculator-theiving", {})
+  });
+
+  app.get('/skill-calculator-crafting', function(req, res, next) {
+    res.render("skill-calculator-crafting", {})
+  });
+
+  app.get('/skill-calculator-fletching', function(req, res, next) {
+    res.render("skill-calculator-fletching", {})
+  });
+
+  app.get('/skill-calculator-slayer', function(req, res, next) {
+    res.render("skill-calculator-slayer", {})
+  });
+  
+  app.get('/skill-calculator-hunter', function(req, res, next) {
+    res.render("skill-calculator-hunter", {})
+  });
+
+  app.get('/skill-calculator-mining', function(req, res, next) {
+    res.render("skill-calculator-mining", {})
+  });
+
+  app.get('/skill-calculator-smithing', function(req, res, next) {
+    res.render("skill-calculator-smithing", {})
+  });
+
+  app.get('/skill-calculator-fishing', function(req, res, next) {
+    res.render("skill-calculator-fishing", {})
+  });
+
+  app.get('/skill-calculator-cooking', function(req, res, next) {
+    res.render("skill-calculator-cooking", {})
+  });
+
+  app.get('/skill-calculator-firemaking', function(req, res, next) {
+    res.render("skill-calculator-firemaking", {})
+  });
+
+  app.get('/skill-calculator-woodcutting', function(req, res, next) {
+    res.render("skill-calculator-woodcutting", {})
+  });
+
+  app.get('/skill-calculator-farming', function(req, res, next) {
+    res.render("skill-calculator-farming", {})
+  });
+
 app.get('/bug-reporter', function(req, res, next) {
   res.render("bug-reporter", {})
 });
