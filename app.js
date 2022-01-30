8fb8949622fbd03a5f37f246eb118d237ea65023
Trenton Kress <trenton.kress@gmail.com>
2022-01-30 11:16:16 -0800

    Add HTTPS support

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
+const config = require('./config.json');
+const createError = require('http-errors');
+const https = require('https');
+const fs = require('fs');
+const express = require('express');
+const path = require('path');
 express.static.mime.types['wasm'] = 'application/wasm'
 
-var app = express();
-const webPort = 5555;
+const app = express();
+const webPort = config.port;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -26,160 +29,166 @@ app.post('/posty', (request, response) => {
 })
 
 app.get('/create-account', function(req, res, next) {
-  res.render("create-account", {})
+  res.render('create-account', {})
 });
 
 app.get('/login', function(req, res, next) {
-  res.render("login", {})
+  res.render('login', {})
 });
 
 app.get('/download', function(req, res, next) {
-  res.render("download", {})
+  res.render('download', {})
 });
 
 app.get('/features', function(req, res, next) {
-  res.render("features", {})
+  res.render('features', {})
 });
 
 app.get('/guides', function(req, res, next) {
-  res.render("guides", {})
+  res.render('guides', {})
 });
 
 app.get('/highscores', function(req, res, next) {
-  res.render("highscores", {})
+  res.render('highscores', {})
 });
 
 app.get('/tools', function(req, res, next) {
-  res.render("tools", {})
+  res.render('tools', {})
 });
 
 app.get('/ge-tracker', function(req, res, next) {
-  res.render("ge-tracker", {})
+  res.render('ge-tracker', {})
 });
 
 app.get('/skill-calculator', function(req, res, next) {
-  res.render("skill-calculator", {})
+  res.render('skill-calculator', {})
 });
 
   app.get('/skill-calculator-attack', function(req, res, next) {
-    res.render("skill-calculator-attack", {})
+    res.render('skill-calculator-attack', {})
   });
 
   app.get('/skill-calculator-strength', function(req, res, next) {
-    res.render("skill-calculator-strength", {})
+    res.render('skill-calculator-strength', {})
   });
 
   app.get('/skill-calculator-defense', function(req, res, next) {
-    res.render("skill-calculator-defense", {})
+    res.render('skill-calculator-defense', {})
   });
 
   app.get('/skill-calculator-ranged', function(req, res, next) {
-    res.render("skill-calculator-ranged", {})
+    res.render('skill-calculator-ranged', {})
   });
 
   app.get('/skill-calculator-magic', function(req, res, next) {
-    res.render("skill-calculator-magic", {})
+    res.render('skill-calculator-magic', {})
   });
 
   app.get('/skill-calculator-prayer', function(req, res, next) {
-    res.render("skill-calculator-prayer", {})
+    res.render('skill-calculator-prayer', {})
   });
 
   app.get('/skill-calculator-summoning', function(req, res, next) {
-    res.render("skill-calculator-summoning", {})
+    res.render('skill-calculator-summoning', {})
   });
 
   app.get('/skill-calculator-runecrafting', function(req, res, next) {
-    res.render("skill-calculator-runecrafting", {})
+    res.render('skill-calculator-runecrafting', {})
   });
 
   app.get('/skill-calculator-construction', function(req, res, next) {
-    res.render("skill-calculator-construction", {})
+    res.render('skill-calculator-construction', {})
   });
 
   app.get('/skill-calculator-dungeoneering', function(req, res, next) {
-    res.render("skill-calculator-dungeoneering", {})
+    res.render('skill-calculator-dungeoneering', {})
   });
 
   app.get('/skill-calculator-agility', function(req, res, next) {
-    res.render("skill-calculator-agility", {})
+    res.render('skill-calculator-agility', {})
   });
 
   app.get('/skill-calculator-herblore', function(req, res, next) {
-    res.render("skill-calculator-herblore", {})
+    res.render('skill-calculator-herblore', {})
   });
 
   app.get('/skill-calculator-theiving', function(req, res, next) {
-    res.render("skill-calculator-theiving", {})
+    res.render('skill-calculator-theiving', {})
   });
 
   app.get('/skill-calculator-crafting', function(req, res, next) {
-    res.render("skill-calculator-crafting", {})
+    res.render('skill-calculator-crafting', {})
   });
 
   app.get('/skill-calculator-fletching', function(req, res, next) {
-    res.render("skill-calculator-fletching", {})
+    res.render('skill-calculator-fletching', {})
   });
 
   app.get('/skill-calculator-slayer', function(req, res, next) {
-    res.render("skill-calculator-slayer", {})
+    res.render('skill-calculator-slayer', {})
   });
   
   app.get('/skill-calculator-hunter', function(req, res, next) {
-    res.render("skill-calculator-hunter", {})
+    res.render('skill-calculator-hunter', {})
   });
 
   app.get('/skill-calculator-mining', function(req, res, next) {
-    res.render("skill-calculator-mining", {})
+    res.render('skill-calculator-mining', {})
   });
 
   app.get('/skill-calculator-smithing', function(req, res, next) {
-    res.render("skill-calculator-smithing", {})
+    res.render('skill-calculator-smithing', {})
   });
 
   app.get('/skill-calculator-fishing', function(req, res, next) {
-    res.render("skill-calculator-fishing", {})
+    res.render('skill-calculator-fishing', {})
   });
 
   app.get('/skill-calculator-cooking', function(req, res, next) {
-    res.render("skill-calculator-cooking", {})
+    res.render('skill-calculator-cooking', {})
   });
 
   app.get('/skill-calculator-firemaking', function(req, res, next) {
-    res.render("skill-calculator-firemaking", {})
+    res.render('skill-calculator-firemaking', {})
   });
 
   app.get('/skill-calculator-woodcutting', function(req, res, next) {
-    res.render("skill-calculator-woodcutting", {})
+    res.render('skill-calculator-woodcutting', {})
   });
 
   app.get('/skill-calculator-farming', function(req, res, next) {
-    res.render("skill-calculator-farming", {})
+    res.render('skill-calculator-farming', {})
   });
 
 app.get('/bug-reporter', function(req, res, next) {
-  res.render("bug-reporter", {})
+  res.render('bug-reporter', {})
 });
 
 app.get('/world-map', function(req, res, next) {
-  res.render("world-map", {})
+  res.render('world-map', {})
 });
 
 app.get('/api', function(req, res, next) {
-  res.render("api", {})
+  res.render('api', {})
 });
 
 app.get('/terms', function(req, res, next) {
-  res.render("terms", {})
+  res.render('terms', {})
 });
 
 app.get('/privacy-policy', function(req, res, next) {
-  res.render("privacy-policy", {})
+  res.render('privacy-policy', {})
 });
 
-app.listen(webPort, "0.0.0.0", ()=> console.log("Successful on port " + webPort))
+if (config.https) {
+  const httpsServer = https.createServer({
+    key: fs.readFileSync(config.keyPath),
+    cert: fs.readFileSync(config.certPath),
+  }, app);
 
-
-
+  httpsServer.listen(443, '0.0.0.0', () => console.log('HTTPS successful on port 443.'));
+} else {
+  app.listen(webPort, '0.0.0.0', () => console.log('HTTP successful on port ' + webPort));
+}
 
 module.exports = app;
