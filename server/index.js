ca9bebf41f34d7c7b7180b7982a6bc3dac571d30
Trenton Kress <trenton.kress@gmail.com>
2022-02-02 18:52:29 -0800

    formatting

--- server/index.js
+++ server/index.js
@@ -33,9 +33,7 @@ server.get('/*', (req, res) => {
       return res.status(500).send('Oops, better luck next time!');
     }
 
-    return res.send(
-      data.replace('<div id="root"></div>', `<div id="root">${app}</div>`)
-    );
+    return res.send(data.replace('<div id="root"></div>', `<div id="root">${app}</div>`));
   });
 });
 
